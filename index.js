bf95ef6a236802303b5808c0ea6fb0c4b2be14d2
Szymon Grabowski <szmiko@wp.pl>
2018-05-16 17:28:20 +0200

    Task

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ var movies = [
 		id: 1,
 		title: 'Harry Potter',
 		desc: 'film o czarodzieju',
-		src: 'http://www.filmweb.pl/Harry.Potter.I.Kamien.Filozoficzny'
+		src: 'http://1.fwcdn.pl/po/05/71/30571/7529392.6.jpg'
 	},
 	{
 		id: 2,
 		title: 'Król lew',
 		desc: 'Film o lwie',
-		src: 'http://www.filmweb.pl/Krol.Lew'
+		src: 'http://1.fwcdn.pl/po/68/78/6878/6927221.6.jpg'
 	},
 	{
 		id: 3,
 		title: 'Gniew oceanu',
 		desc: 'Film o rybakach',
-		src: 'http://www.filmweb.pl/film/Gniew+oceanu-2000-954'
+		src: 'http://1.fwcdn.pl/po/09/54/954/7518080.6.jpg'
 	},
 	{
 		id: 4,
 		title: 'Siedem',
 		desc: 'Film o seryjnym mordercy',
-		src: 'http://www.filmweb.pl/Siedem'
+		src: 'http://1.fwcdn.pl/po/07/02/702/6967799.3.jpg'
 	}
 ];
 
@@ -31,14 +31,16 @@ var Movie = React.createClass({
 	},
 
 	render: function() {
-		return (
-			React.createElement('div', {},
-				React.createElement('li', {key: movies.id}),
-				React.createElement('h2', {}, this.props.movies.title),
-				React.createElement('p', {}, this.props.movies.desc),
-				React.createElement('img', {src: this.props.movies.src})
+		var moviesElements = movies.map(function(movie) {
+			return (
+				React.createElement('div', {},
+					React.createElement('li', {key: movies.id}),
+					React.createElement('h2', {}, this.props.movies.title),
+					React.createElement('p', {}, this.props.movies.desc),
+					React.createElement('img', {src: this.props.movies.src, alt: movies.desc})
+				)
 			)
-		)
+		})
 	}
 });
 
